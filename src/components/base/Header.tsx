aedcd1142fa1dcff825647d25f8429a39f8af7e9
huylongnguyendev <nguyenhuylong.dev@gmail.com>
2026-08-04 09:40:22 -0400

    add header

---
+++ src/components/base/Header.tsx
@@ -0,0 +1,24 @@
+import { useMatches } from "@tanstack/react-router";
+import { Button } from "../ui/button";
+import { MenuIcon } from "lucide-react";
+
+export function Header() {
+  const matches = useMatches();
+  const currentName = matches.some((s) => s.staticData?.title);
+  const isShowHeader = !matches.some((s) => s.staticData?.isShowHeader);
+
+  return (
+    <>
+      {isShowHeader ? (
+        <header className="sticky top-0 left-0 z-9999 bg-background/80 backdrop-blur-sm p-4 shadow-md flex justify-between items-center">
+          <Button variant={"outline"} size={"icon"}>
+            <MenuIcon />
+          </Button>
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-1/2 text-center md:text-xl font-semibold uppercase text-primary cursor-default">
+            {currentName || "Toán học"}
+          </h1>
+        </header>
+      ) : null}
+    </>
+  );
+}
